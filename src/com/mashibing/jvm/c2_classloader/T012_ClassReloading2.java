9d6aeb94eefa5c03921bfec865fc4d3f189df9fc
闪狙手 <s0181@163.com>
2022-04-18 11:15:59 +0800

    + .gitignore

--- src/com/mashibing/jvm/c2_classloader/T012_ClassReloading2.java
+++ src/com/mashibing/jvm/c2_classloader/T012_ClassReloading2.java
@@ -1,7 +1,5 @@
 package com.mashibing.jvm.c2_classloader;
 
-import com.mashibing.jvm.Hello;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,22 +9,16 @@ public class T012_ClassReloading2 {
     private static class MyLoader extends ClassLoader {
         @Override
         public Class<?> loadClass(String name) throws ClassNotFoundException {
-
-            File f = new File("C:/work/ijprojects/JVM/out/production/JVM/" + name.replace(".", "/").concat(".class"));
-
-            if(!f.exists()) return super.loadClass(name);
-
+            File f = new File("C:/ws/Github-klines/04Thread/jvm-notes-demos/target/classes/" + name.replace(".", "/").concat(".class"));
+            if (!f.exists()) return super.loadClass(name);
             try {
-
                 InputStream is = new FileInputStream(f);
-
                 byte[] b = new byte[is.available()];
                 is.read(b);
                 return defineClass(name, b, 0, b.length);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return super.loadClass(name);
         }
     }
@@ -34,10 +26,8 @@ public class T012_ClassReloading2 {
     public static void main(String[] args) throws Exception {
         MyLoader m = new MyLoader();
         Class clazz = m.loadClass("com.mashibing.jvm.Hello");
-
         m = new MyLoader();
         Class clazzNew = m.loadClass("com.mashibing.jvm.Hello");
-
         System.out.println(clazz == clazzNew);
     }
 }
